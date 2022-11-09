c7a86b9788984170c395557f7db3447db50433a5
Visitrack SAS <visitracksas@gmail.com>
2022-11-09 08:39:06 -0500

    commit 09 nov 2022

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ module.exports.io = SokectIO(server);
 require('./Sockets/socket');
 require('./Jobs/UsersEntrena.js');
 
-console.log('hola mundo en visitrack 2.0')
+console.log('hola mundo en visitrack 2.0 nov')
 
 mongoose.connect('mongodb://localhost:27017/repository', { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
     if (err) {
