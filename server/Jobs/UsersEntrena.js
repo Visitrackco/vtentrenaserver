0cefcb8fac1396339a2c0c712b640ac794ade1d6
SantiagoFernandezdev <santigovelascofernandez@iMac-de-Santigo.local>
2022-11-21 23:02:33 -0500

    21 nov 2022

--- server/Jobs/UsersEntrena.js
+++ server/Jobs/UsersEntrena.js
@@ -1,7 +1,7 @@
 const UsersBasicShecma = require('../Models/EntrenaVTModels/UsersBasicsEntrena');
 const moment = require('moment-timezone');
 const job = require('node-cron');
-
+const axios = require('axios');
 const myJob = job.schedule('* * * * *', () => {
      UsersBasicShecma.find({status: 'on'})
      .exec((error, documents) => {
@@ -33,6 +33,76 @@ const myJob = job.schedule('* * * * *', () => {
                })
           }
      })
+})
+
+async function fnchilds(guid) {
+    try {
+     const resp = await axios.post('https://api.visitrack.com/api/Surveys/ActivityChilds', {
+          AccessToken: '68C5A337B89F6B7A603861D440EB20D6',
+
+          ParentGUID: guid,
+     });
+
+     return resp;
+    } catch (error) {
+     return [];
+    }     
+}
+
+async function fnForm(guid) {
+     try {
+          const resp = await axios.post('https://api.visitrack.com/api/Surveys/Activities', {
+               AccessToken: '68C5A337B89F6B7A603861D440EB20D6',
+               From: moment().subtract(2, 'days').format('YYYY-MM-DD HH:mm'),
+               To: moment().format('YYYY-MM-DD HH:mm'),
+               FormGUID: guid,
+          });
+     
+          if (resp) {
+    
+               resp.data.forEach(async (item) => {
+                    if (item.CompanyStatusName == 'SOLICITUD ENVIADA AL TECNICO' ) {
+                         const childs = await fnchilds(item.GUID)
+     
+                         if (childs.data.length > 0) {
+                              console.log('eliminado ', item.GUID)
+                              const elimi = await axios.post('https://api.visitrack.com/api/Surveys/MovilDelete', {
+                                   AccessToken: '68C5A337B89F6B7A603861D440EB20D6',
+                                   ActivityGUID: item.GUID
+                              });
+                              console.log(elimi.data)
+                         }
+                    } else if (item.CompanyStatusName == 'MANTENIMIENTO FINALIZADO') {
+
+                         const elimi = await axios.post('https://api.visitrack.com/api/Surveys/MovilDelete', {
+                              AccessToken: '68C5A337B89F6B7A603861D440EB20D6',
+                              ActivityGUID: item.GUID
+                         });
+
+                    }
+               })
+          }
+     
+          return resp;
+     } catch (error) {
+      return [];
+     }     
+ }
+
+const eliminado = job.schedule(' */5 * * * *', async () => {
+
+     console.log('Hola', moment().subtract(2, 'days').format('YYYY-MM-DD HH:mm'))
+
+     try {
+          
+          const solicitud1 = await fnForm('BAB8F522-721F-4E63-BF9F-C99F2CB78AC6')
+          const solicitud2 = await fnForm('02CEE670-587E-49CA-A2CC-C10B1D519F65')
+          const solicitud3 = await fnForm('76E93F88-7612-466E-BBD7-3C95A7679D6D')
+      
+      } catch (err) {
+          console.log(err)
+      }
+   
 })
 
                
