bc61034c15f34b32b32fb8ab4352d7c2ab6d521e
SantiagoFernandezdev <santigovelascofernandez@iMac-de-Santigo.local>
2022-12-29 19:40:33 -0500

    commit 29 dic 2022Â

--- server/Jobs/UsersEntrena.js
+++ server/Jobs/UsersEntrena.js
@@ -762,6 +762,7 @@ function fnDiamante() {
                         if (diff == 7 || diff == 1) {
                             console.log('enviar')
 
+                            
 
                             const body = `<div style=" width: 600px; margin: 20px auto; padding: 20px; border-radius: 20px; background-color: #E2F0FD; font-family: sans-serif; height: 600px;">
                             <h3 style="text-align: center; font-size: 32px; margin-bottom: 100px;">Mantenimiento Preventivo</h3>
